96dccb2269e82965acf412b78927c7c0199f6168
Andrey Valyaev <dron.valyaev@gmail.com>
2016-08-29 23:53:59 +0300

    createproject view by #3

---
+++ ui/tests/testProjects.py
@@ -0,0 +1,11 @@
+from django.test import Client, TestCase
+from base.models import Project
+
+class TestProject(TestCase):
+	def testProjectShouldBeCreateOverForm(self):
+		# When
+		Project.objects.all().delete()
+		response = Client().post('/ui/createproject', data={'name':'test_project'})
+		# Then
+		self.assertEqual(response.status_code, 200)
+		Project.objects.get(name='test_project')	# Not raise
--- ui/views.py
+++ ui/views.py
@@ -1,4 +1,13 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import View
+from base.models import Project
 
 def index(request):
 	return render(request, 'ui/index.html', {})
+
+
+class CreateProjectView(View):
+	def post(self, request, *args, **kwargs):
+		Project.objects.create(name=request.POST['name'])
+		return HttpResponse()
