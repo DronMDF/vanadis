90b2ea575530a133a41cc444023ed19953cd607e
Andrey Valyaev <dron.valyaev@gmail.com>
2016-09-01 14:06:01 +0300

    Testing with postgresql by #6

--- vanadis/settings.py
+++ vanadis/settings.py
@@ -75,12 +75,23 @@ WSGI_APPLICATION = 'vanadis.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-DATABASES = {
-	'default': {
-		'ENGINE': 'django.db.backends.sqlite3',
-		'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if os.getenv('DATABASE') == 'postgresql':
+	DATABASES = {
+		'default': {
+			'ENGINE': 'django.db.backends.postgresql',
+			'NAME': 'vanadisdb',
+			'USER': 'vanadis',
+			'PASSWORD': 'vanadis',
+			'HOST': 'localhost',
+		}
 	}
-}
+else:
+	DATABASES = {
+		'default': {
+			'ENGINE': 'django.db.backends.sqlite3',
+			'NAME': os.path.join(BASE_DIR, 'vanadisdb.sqlite3'),
+		},
+	}
 
 
 # Password validation
