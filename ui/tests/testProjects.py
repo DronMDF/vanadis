89eb98cd7f65271d3a3ceef9a2572efbdb5d9491
Andrey Valyaev <dron.valyaev@gmail.com>
2016-09-05 11:38:38 +0300

    Report upload form by #16

--- ui/tests/testProjects.py
+++ ui/tests/testProjects.py
@@ -20,3 +20,16 @@ class TestProject(TestCase):
 		# Then
 		self.assertEqual(response.status_code, 200)
 		self.assertIn('named', response.content.decode('utf-8'))
+
+	def testProjectPageContainReportUploadForm(self):
+		# Given
+		project = Project.objects.create()
+		# When
+		response = Client().get('/ui/project/%u/' % project.id)
+		# Then
+		self.assertEqual(response.status_code, 200)
+		content = response.content.decode('utf-8')
+		self.assertIn("<form action='/import' method='post' enctype='multipart/form-data'>",
+			content)
+		self.assertIn("<input type='hidden' name='project' value='%s'>" % project.name,
+			content)
