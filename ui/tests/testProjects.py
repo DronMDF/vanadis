eeaceb86c17d74d3f96ff94eea60e5d6784e488f
Andrey Valyaev <dron.valyaev@gmail.com>
2016-09-05 18:34:07 +0300

    Project name validation by #19

--- ui/tests/testProjects.py
+++ ui/tests/testProjects.py
@@ -3,9 +3,34 @@ from base.models import Project
 
 
 class TestProject(TestCase):
-	def testProjectShouldBeCreateOverForm(self):
+	def testProjectShouldContainValidChars(self):
 		# When
+		response = Client().post('/ui/project', data={'name': '%letter'})
+		# Then
+		self.assertEqual(response.status_code, 400)
+
+	def testProjectNameShouldBeUnder100Chars(self):
+		# When
+		response = Client().post('/ui/project', data={'name': 'a' * 101})
+		# Then
+		self.assertEqual(response.status_code, 400)
+
+	def testProjectNameRejectIfDigitAtBegin(self):
+		# When
+		response = Client().post('/ui/project', data={'name': '007a'})
+		# Then
+		self.assertEqual(response.status_code, 400)
+
+	def testProjectLongValidNameAreAcepted(self):
+		# When
+		response = Client().post('/ui/project', data={'name': 'x' * 100})
+		# Then
+		self.assertEqual(response.status_code, 302)		# Ok
+
+	def testProjectShouldBeCreateOverForm(self):
+		# Given
 		Project.objects.all().delete()
+		# When
 		response = Client().post('/ui/project', data={'name': 'test_project'})
 		# Then
 		self.assertEqual(response.status_code, 302)
--- ui/views.py
+++ ui/views.py
@@ -1,4 +1,5 @@
-from django.http import HttpResponse
+import re
+from django.http import HttpResponseBadRequest
 from django.shortcuts import redirect, render, get_object_or_404
 from django.views.decorators.csrf import csrf_protect
 from django.views.decorators.http import require_GET, require_POST
@@ -22,5 +23,8 @@ def project(request, id):
 @require_POST
 @csrf_protect
 def createProject(request):
-	Project.objects.create(name=request.POST['name'])
+	name = request.POST['name']
+	if not re.match('^[a-zA-Z]{1}[\w-]{1,98}[a-zA-Z0-9]{1}$', name):
+		return HttpResponseBadRequest()
+	Project.objects.create(name=name)
 	return redirect('/ui')
