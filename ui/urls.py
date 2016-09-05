eeb15fd442d7fae06b19f977e7bf9a068673faa1
Andrey Valyaev <dron.valyaev@gmail.com>
2016-09-05 17:53:53 +0300

    create project over POST (by REST convention) by #19

--- ui/urls.py
+++ ui/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
-	url(r'^createproject$', views.CreateProjectView.as_view()),
+	url(r'^project$', views.createProject),
 	url(r'^project/(?P<id>\d+)/$', views.project),
 ]
--- ui/views.py
+++ ui/views.py
@@ -1,29 +1,26 @@
 from django.http import HttpResponse
 from django.shortcuts import redirect, render, get_object_or_404
-from django.views import View
+from django.views.decorators.csrf import csrf_protect
+from django.views.decorators.http import require_GET, require_POST
 from base.models import Project
 
 
+@require_GET
 def index(request):
 	projects = Project.objects.all()
 	context = {'projects': projects}
 	return render(request, 'ui/index.html', context)
 
 
+@require_GET
 def project(request, id):
 	project = get_object_or_404(Project, id=id)
 	context = {'project': project, 'revisions': [], 'branches': [], 'files': []}
 	return render(request, 'ui/project.html', context)
 
 
-class CreateProjectView(View):
-	def createProject(self, name):
-		Project.objects.create(name=name)
-
-	def post(self, request, *args, **kwargs):
-		self.createProject(request.POST['name'])
-		return redirect('/ui')
-
-	def get(self, request, *args, **kwargs):
-		self.createProject(request.GET['name'])
-		return redirect('/ui')
+@require_POST
+@csrf_protect
+def createProject(request):
+	Project.objects.create(name=request.POST['name'])
+	return redirect('/ui')
