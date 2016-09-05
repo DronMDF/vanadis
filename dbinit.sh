d44d0fef7e1dcf8300e95a8ed15b8c51913507e7
Andrey Valyaev <dron.valyaev@gmail.com>
2016-09-05 10:24:23 +0300

    Separate database settings by #8

---
+++ dbinit.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+if [ "${DATABASE}" = "postgresql" ]; then
+	echo "PostgreSQL database..."
+	pip install --upgrade wheel
+	pip install psycopg2
+	psql -c "create role vanadis with login createdb password 'vanadis';" -U postgres
+	psql -c "create database vanadisdb;" -U postgres
+else
+	echo "Sqlite database..."
+fi
