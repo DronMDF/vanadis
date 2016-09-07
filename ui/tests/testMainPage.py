c11db883fa84d1fb4eb77ebbd2d8427bdcf08df1
Andrey Valyaev <dron.valyaev@gmail.com>
2016-09-07 22:24:59 +0300

    Issue count next to the project on the main page by #12

--- ui/tests/testMainPage.py
+++ ui/tests/testMainPage.py
@@ -1,5 +1,5 @@
 from django.test import Client, TestCase
-from base.models import Project
+from base.models import Issue, Project
 
 
 class TestMainPage(TestCase):
@@ -22,3 +22,19 @@ class TestMainPage(TestCase):
 		text = response.content.decode('utf-8')
 		self.assertIn('test1', text)
 		self.assertIn('test2', text)
+
+	def testProjectProblemCounter(self):
+		# Given
+		Project.objects.all().delete()
+		tp1 = Project.objects.create(name='test1')
+		tp2 = Project.objects.create(name='test2')
+		Issue.objects.create(project=tp1, line=0, position=0)
+		Issue.objects.create(project=tp1, line=0, position=0)
+		Issue.objects.create(project=tp2, line=0, position=0)
+		# When
+		response = Client().get('/ui/')
+		# Then
+		self.assertEqual(response.status_code, 200)
+		text = response.content.decode('utf-8')
+		self.assertRegex(text, 'test1[^1]+2')		# 2 issue
+		self.assertRegex(text, 'test2[^2]+1')		# 1 issue
