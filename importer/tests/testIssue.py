6e8cd12d886195f8fd80c18d0440e24a1156693d
Andrey Valyaev <dron.valyaev@gmail.com>
2016-09-08 23:02:32 +0300

    Fix pep8 by #28

--- importer/tests/testIssue.py
+++ importer/tests/testIssue.py
@@ -5,11 +5,13 @@ from importer.issue import *
 class TestIssueChain(TestCase):
 	def testIgnoreSplitLogToChain(self):
 		# Given
-		log = [b'header',
+		log = [
+			b'header',
 			b'a.c:1:3: warning: msg', b'code', b'note',
 			b'b.c:5:7: warning: xxx', b'dummy']
 		# When
 		result = list(splitReportToIssueChain(log))
-		self.assertListEqual(result, [[b'header'],
+		self.assertListEqual(result, [
+			[b'header'],
 			[b'a.c:1:3: warning: msg', b'code', b'note'],
 			[b'b.c:5:7: warning: xxx', b'dummy']])
