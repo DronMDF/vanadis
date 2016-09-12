562274ee9d1a32bab7f88256c9c71386dd940ef2
Andrey Valyaev <dron.valyaev@gmail.com>
2016-09-12 16:54:42 +0300

    Shows several errors under one line by #45

---
+++ ui/tests/testFileView.py
@@ -0,0 +1,21 @@
+from django.test import Client, TestCase
+from base.models import Issue, Project
+
+
+class TestFileView(TestCase):
+	def testViewShouldReturnByLineContext(self):
+		# Given
+		Project.objects.all().delete()
+		p = Project.objects.create(name='ff')
+		Issue.objects.create(project=p, file='xxx', code='aaa',
+				line=7, position=5, text='All bad')
+		Issue.objects.create(project=p, file='xxx', code='aaa',
+				line=7, position=10, text='End bad')
+		# When
+		response = Client().get('/ui/project/ff/xxx')
+		# Then
+		sc = response.context['sourcecode']
+		self.assertEqual(sc[0]['code'], 'aaa')
+		self.assertEqual(sc[0]['line'], 7)
+		self.assertEqual(sc[0]['issues'][0]['position'], 10)
+		self.assertEqual(sc[0]['issues'][1]['position'], 5)
