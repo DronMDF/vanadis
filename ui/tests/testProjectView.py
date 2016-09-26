52299c611a6ed71305159fb7e7aa67c5c2eb6b9f
Andrey Valyaev <dron.valyaev@gmail.com>
2016-09-26 22:37:07 +0300

    href from Project page to Settings by #72

--- ui/tests/testProjectView.py
+++ ui/tests/testProjectView.py
@@ -2,12 +2,22 @@ from django.test import Client, TestCase
 from base.models import Project
 
 
-class TestProjectView(TestCase):
+class TestProjectSettingsView(TestCase):
 	def testProjectSettingView(self):
 		# Given
 		Project.objects.all().delete()
 		p = Project.objects.create(name='setp')
 		# When
-		reply = Client().get('/ui/project/setp?setting')
+		reply = Client().get('/ui/setp/settings')
 		# Then
 		self.assertEqual(reply.status_code, 200)
+
+	def testProjectSettingsUrl(self):
+		# Given
+		Project.objects.all().delete()
+		p = Project.objects.create(name='setp')
+		# When
+		reply = Client().get('/ui/project/setp/')
+		# Then
+		self.assertEqual(reply.status_code, 200)
+		self.assertIn("href='/ui/setp/settings'", reply.content.decode('utf-8'))
