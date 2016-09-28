b269328fd0afe2febf749581c56b492ef8b92f87
Andrey Valyaev <dron.valyaev@gmail.com>
2016-09-28 16:04:37 +0300

    Install pylint by #77

--- style.sh
+++ style.sh
@@ -2,7 +2,7 @@
 set -e
 find ./ -name "*.py" | xargs pep8 --show-source --max-line-length=100 --ignore=W191,E128
 # TODO: Need to reduce disabled list
-find ./ -name "*.py" | xargs pylint3 --max-line-length=100 --indent-string="	" -fparseable \
+find ./ -name "*.py" | xargs pylint --max-line-length=100 --indent-string="	" -fparseable \
 	-e all -d missing-docstring,invalid-name,bad-continuation,no-member,too-many-ancestors \
 	-d too-few-public-methods,no-self-use,wildcard-import,duplicate-code,too-many-arguments \
 	-d super-init-not-called,locally-disabled,suppressed-message -r no
