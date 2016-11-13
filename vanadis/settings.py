12353e59d3994fcf48d6470cfaebcef69755a293
Andrey Valyaev <dron.valyaev@gmail.com>
2016-11-13 21:18:33 +0300

    Allow any host by #96

--- vanadis/settings.py
+++ vanadis/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = '-4y+7$ps7#oves)1rik$^wx$^xj$atj#l43vje#emmb4p*$zq^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
