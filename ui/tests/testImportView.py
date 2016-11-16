810541c2ac1b5078d283582f31232bb828400db5
Andrey Valyaev <dron.valyaev@gmail.com>
2016-11-16 23:01:53 +0300

    #92: Check revision by reposytory

--- ui/tests/testImportView.py
+++ ui/tests/testImportView.py
@@ -1,10 +1,15 @@
 from django.test import RequestFactory, TestCase
+from django.http.response import Http404
 from ui.views import ImportView
 from base.models import Issue, Project
 
 
 class ImportViewUT(ImportView):
-	pass
+	def getRevision(self, project, revision):
+		rm = {'11111111': 0x11111111, '22222222': 0x22222222}
+		if revision not in rm:
+			raise Http404('No Revision')
+		return rm[revision]
 
 
 class TestImportView(TestCase):
@@ -30,9 +35,18 @@ class TestImportView(TestCase):
 		request = self.factory.post('/import1/import/0', content_type='application/xml',
 			data=request_body)
 		# When
-		ImportViewUT.as_view()(request, projectname='import1')
+		ImportViewUT.as_view()(request, projectname='import1', revision='11111111')
 		# Then
 		issue = Issue.objects.filter(project__name='import1')[0]
 		self.assertEqual(issue.file.path, 'xxx.c')
 		self.assertEqual(issue.line, 123)
 		self.assertEqual(issue.text, 'xxx')
+
+	def testMissingRevisionCause404(self):
+		# Given
+		Project.objects.create(name='norev')
+		request = self.factory.post('/norev/import/12345678',
+			content_type='application/xml', data='<files/>')
+		# When
+		with self.assertRaises(Http404):
+			ImportViewUT.as_view()(request, projectname='norev', revision='12345678')
--- ui/views/ImportView.py
+++ ui/views/ImportView.py
@@ -1,17 +1,27 @@
 from xml.etree import ElementTree
-from django.http.response import HttpResponse
+from django.http.response import Http404, HttpResponse
 from django.shortcuts import get_object_or_404
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import View
 from base.models import File, Issue, Project
+from importer import Repository
 
 
 @method_decorator(csrf_exempt, name='dispatch')
 class ImportView(View):
+	def getRevision(self, project, revision):
+		try:
+			repository = Repository(project)
+			return repository.revparse(revision)
+		except:
+			raise Http404("Revision was not found")
+
 	def post(self, request, **kwargs):
 		projectname = kwargs['projectname']
 		project = get_object_or_404(Project, name=projectname)
+
+		revision = self.getRevision(project, kwargs['revision'])
 
 		it = ElementTree.parse(request)
 		issues = []
@@ -21,7 +31,7 @@ class ImportView(View):
 				issues.append(Issue(project=project, file=file,
 					line=int(i.findtext('./line')),
 					position=int(i.findtext('./position', default=0)),
-					text=i.findtext('./message'), code='null'))
+					text=i.findtext('./message'), code=hex(revision)))
 		Issue.objects.bulk_create(issues)
 
 		return HttpResponse()
