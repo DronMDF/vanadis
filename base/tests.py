f333a07196597123de9fc26b8ac0414b41450d4a
Andrey Valyaev <dron.valyaev@gmail.com>
2016-12-21 22:50:30 +0300

    #145: Check for object deletion with project

--- base/tests.py
+++ base/tests.py
@@ -1,1 +1,13 @@
-# Create your tests here.
+from django.test import TestCase
+from base.models import Object, Project
+
+
+class TestModels(TestCase):
+	def testObjectCleanupOnProjectDelete(self):
+		# Given
+		project = Project.objects.create(name='deleted')
+		Object.objects.create(project=project, oid=777, issues_count=0)
+		# When
+		project.delete()
+		# Then
+		self.assertEqual(Object.objects.filter(oid=777).count(), 0)
